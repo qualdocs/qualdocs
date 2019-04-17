19ef9b0295c4097facc03125632a781920af2a66
Stuart Geiger <sgeiger@gmail.com>
2019-04-17 13:59:55 -0700

    v 0.1.0

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='qualdocs',
-      version='0.0.9',
+      version='0.1.0',
       description='Qualitative data analysis based on Google Docs comments',
       url='http://github.com/qualdocs/qualdocs',
       author='Stuart Geiger',
       author_email='stuart@stuartgeiger.com',
+      long_description='A python library for supporting open qualitative coding of text data in Google Docs comments. Integrates with the Google Docs API via PyDrive to create pandas dataframes for each code/tag and the highlighted text in a set of documents.',
       license='MIT',
       packages=['qualdocs'],
       install_requires=[
